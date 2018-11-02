de96fa31c0121f13b1d0326b68d807192f8a29c8
bartek <sawicki.bartek@hotmail.com>
2018-11-02 12:10:08 +0100

    Implement interfaces for services. Close #5.

---
+++ pet-clinic-data/src/main/java/com/bartek/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.bartek.sfgpetclinic.services;
+
+import com.bartek.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/bartek/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.bartek.sfgpetclinic.services;
+
+import com.bartek.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
