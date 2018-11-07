0f983e061f4ddad13a4dee96dbb61f12afb9a9dd
bartek <sawicki.bartek@hotmail.com>
2018-11-07 13:16:53 +0100

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-web/src/main/java/com/bartek/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/bartek/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Jan");
         owner1.setLastName("Kowalski");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Shrek");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Marian");
         vet1.setLastName("Petlover");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Zbyszek");
         vet2.setLastName("Weterynarz");
 
